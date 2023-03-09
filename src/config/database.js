2bdf6c81e3048d141dc95f78ad22c15b672e3558
Anugrah Ilhami Rizki <anugrahilhamirizki@gmail.com>
2023-03-09 19:42:41 +0700

    fix: Added database host

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 
 // Connect to the database
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
     dialect: "mysql",
     logging: false
 });
