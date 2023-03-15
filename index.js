d7c471ed760cac1f4fa210cef2bf2583233a0de1
Anugrah Ilhami Rizki <anugrahilhamirizki@gmail.com>
2023-03-15 20:32:01 +0700

    DEPLOY fix: remove CORS middleware

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const sequelize = require('./src/config/database');
 const routes = require('./src/routes/index');
 const port = process.env.PORT;
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin: ['https://cultour1.vercel.app', 'http://localhost:5173', 'http://localhost:3000'],
-    methods: ['POST', 'GET', 'PUT', 'DELETE'],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin']
-}));
-
 
 // Synchronize database every new commit
 // Replace with 'sequelize.sync({ force: true })' to delete all existing data in database
